9c90e1f1bca8adddf595e9d697a006bedee2ed3e
Thomas Kalbacher <info@opengeosys.org>
2013-02-13 13:20:41 +0100

    LinearInterpolation::getValue: Interpolation for ascending and descending curves

--- ogsCore/MathLib/Interpolation/LinearInterpolation.cpp
+++ ogsCore/MathLib/Interpolation/LinearInterpolation.cpp
@@ -49,31 +49,73 @@ LinearInterpolation::~LinearInterpolation()
 
 double LinearInterpolation::getValue ( double pnt_to_interpolate )
 {
+
     // search interval that has the point inside
     size_t interval_idx (std::numeric_limits<size_t>::max());
+	
     for (size_t k(1); k<_supporting_points.size() && interval_idx == std::numeric_limits<size_t>::max(); k++) {
         if (_supporting_points[k-1] <= pnt_to_interpolate && pnt_to_interpolate <= _supporting_points[k]) {
             interval_idx = k-1;
         }
     }
-
-    // if the index lower than the first entry, 
-    // get the value on the first entry. 
+		
+	// 1st intervall (see for-loop above)
     if ( interval_idx <= 0 )
-        return _values_at_supp_pnts[0];
-    // if the index bigger than the last entry, 
-    // get the value on the last entry. 
+	{
+    // compute linear interpolation polynom: y = m_0 * x + n_0
+    long double m_0 ((_values_at_supp_pnts[1] - _values_at_supp_pnts[0]) / (_supporting_points[1] - _supporting_points[0]));
+    long double n_0 (_values_at_supp_pnts[0] - m_0 * _supporting_points[0]);
+    return m_0 * pnt_to_interpolate + n_0;       
+	}
+	// Out of range (see for-loop above)
     else if (interval_idx >= _supporting_points.size() )
-        return _values_at_supp_pnts[ _supporting_points.size() -1];
-    
+	{
+		//the ascending case
+	    if (_supporting_points[0] <= _supporting_points[_supporting_points.size()-1])
+		{
+			if (pnt_to_interpolate <= _supporting_points[0])
+				return _values_at_supp_pnts[0];
+			else if (pnt_to_interpolate >= _supporting_points[_supporting_points.size()-1])
+				return _values_at_supp_pnts[ _supporting_points.size() -1];
+		}
+		//the descending case
+	    if (_supporting_points[0] > _supporting_points[_supporting_points.size() -1])
+		{
+			if (pnt_to_interpolate >= _supporting_points[0])
+				return _values_at_supp_pnts[0];
+			else if (pnt_to_interpolate <= _supporting_points[_supporting_points.size()-1])
+				return _values_at_supp_pnts[_supporting_points.size() -1];
+		}		
+	}
 
     // compute linear interpolation polynom: y = m * x + n
     long double m ((_values_at_supp_pnts[interval_idx+1] - _values_at_supp_pnts[interval_idx]) / (_supporting_points[interval_idx+1] - _supporting_points[interval_idx]));
-//    double m ((_values_at_supp_pnts[interval_idx] - _values_at_supp_pnts[interval_idx+1]) / (_supporting_points[interval_idx] - _supporting_points[interval_idx+1]));
-//    double n (_values_at_supp_pnts[interval_idx+1] - m * _supporting_points[interval_idx+1]);
     long double n (_values_at_supp_pnts[interval_idx] - m * _supporting_points[interval_idx]);
-
     return m * pnt_to_interpolate + n;
+	
+	//// search interval that has the point inside
+ //   size_t interval_idx (std::numeric_limits<size_t>::max());
+ //   for (size_t k(1); k<_supporting_points.size() && interval_idx == std::numeric_limits<size_t>::max(); k++) {
+ //       if (_supporting_points[k-1] <= pnt_to_interpolate && pnt_to_interpolate <= _supporting_points[k]) {
+ //           interval_idx = k-1;
+ //       }
+ //   }
+ //   // if the index lower than the first entry, 
+ //   // get the value on the first entry. 
+ //   if ( interval_idx <= 0 )
+ //       return _values_at_supp_pnts[0];
+ //   // if the index bigger than the last entry, 
+ //   // get the value of the last entry. 
+ //   else if (interval_idx >= _supporting_points.size() )
+ //       return _values_at_supp_pnts[ _supporting_points.size() -1];
+	//// compute linear interpolation polynom: y = m * x + n
+ //   long double m ((_values_at_supp_pnts[interval_idx+1] - _values_at_supp_pnts[interval_idx]) / (_supporting_points[interval_idx+1] - _supporting_points[interval_idx]));
+ //   //    double m ((_values_at_supp_pnts[interval_idx] - _values_at_supp_pnts[interval_idx+1]) / (_supporting_points[interval_idx] - _supporting_points[interval_idx+1]));
+ //   //    double n (_values_at_supp_pnts[interval_idx+1] - m * _supporting_points[interval_idx+1]);
+ //   long double n (_values_at_supp_pnts[interval_idx] - m * _supporting_points[interval_idx]);
+	//return m * pnt_to_interpolate + n;
+
+
 }
 
 // HS added 18.01.2013
